7c38b87d375459b1120f0f5922699b2885ee0b5b
Soares95 <munizmsb001@gmail.com>
2025-06-01 20:25:49 -0300

    Comentarios e documentacao

---
+++ Calculadora-java/src/comentarios.java
@@ -0,0 +1,21 @@
+public class comentarios {
+ public static void main(String[] args) {
+    //Olá eu sou um comentario em uma unica linha 
+        /*Ola,
+     * Eu sou um comentario
+     * que posso ser mais detalhado
+     * quando necessario
+     */
+    
+    }
+    /**
+     * Estas duas estrelinhas acima
+     * é para identificar que você
+     * pretende elaborar um comentário
+     * a nivel de documentação.
+     * Que incrivel !!!
+    */
+    public void metodo(){
+        
+    }   
+}
