8eb1028e3c8e0982cfba6a3706317480081f11c3
Soares95 <munizmsb001@gmail.com>
2025-06-01 16:40:56 -0300

    Tipos primitivos Java

---
+++ tipos-primitivos/src/ExemploPrimitivos.java
@@ -0,0 +1,13 @@
+public class ExemploPrimitivos {
+    public static void main(String[] args) {
+        int idade = 29;
+        double altura = 1.75;
+        char inicial = 'D';
+        boolean ativo = true;
+
+        System.out.println("Idade: " + idade);
+        System.out.println("Altura: " + altura);
+        System.out.println("Inicial: " + inicial);
+        System.out.println("Ativo: " + ativo);
+    }
+}
