a3f0a654ba4cf0e56a41925c4167cd042a66c123
Soares95 <munizmsb001@gmail.com>
2025-06-01 20:26:31 -0300

    Calculadora java e uso de comentarios

---
+++ Calculadora-java/src/Calculadora.java
@@ -0,0 +1,13 @@
+public class Calculadora {
+    /**
+     * @author Diego Brito
+     * @since 01/06/2024
+     * @param numeroUm
+     * @param numeroDois
+     * @return
+     * @throws Exception
+     */
+    public int somar(int numeroUm, int numeroDois) {
+        return numeroUm + numeroDois;
+    }
+}
